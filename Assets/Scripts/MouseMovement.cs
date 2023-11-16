9d97798c7ecead75bee7a45b730e6f2f1718f502
kirillom <kirill.omelyanchuk@gmail.com>
2023-11-16 14:58:45 +0200

    mouse model and behaviour added

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseMovement : MonoBehaviour
+{
+    public Image monitorCursor;
+    [SerializeField] private int mouseSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(4.2f + monitorCursor.rectTransform.anchoredPosition.x / mouseSpeed, -2, -2f + monitorCursor.rectTransform.anchoredPosition.y / mouseSpeed);
+    }
+}
